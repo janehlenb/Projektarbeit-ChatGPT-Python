202609863e5e6b9339cf64a1d0943a30123e2035
Arveeed <62373688+Arveeed@users.noreply.github.com>
2023-08-17 19:11:23 +0200

    Create Forecast.py

---
+++ Anwendungsprojekt/API/Forecast.py
@@ -0,0 +1,35 @@
+import requests
+
+API_KEY = '8458a13ebaeba1acef15ef61c32b8d4e'
+city_name = 'London'  # Replace with the city name for which you want weather forecast
+
+url = f'http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_KEY}&units=metric'
+
+try:
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        forecast_list = data['list']
+
+        min_temps = []
+        max_temps = []
+        current_temps = []
+
+        for forecast in forecast_list[:5]:  # Extract temperatures for the next 5 days
+            min_temp = forecast['main']['temp_min']
+            max_temp = forecast['main']['temp_max']
+            current_temp = forecast['main']['temp']
+
+            min_temps.append(min_temp)
+            max_temps.append(max_temp)
+            current_temps.append(current_temp)
+
+        print(f"Min Temperatures: {min_temps}")
+        print(f"Max Temperatures: {max_temps}")
+        print(f"Current Temperatures: {current_temps}")
+
+    else:
+        print(f"Error: {data['message']}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
