a9735a35129e60d885cd6cbe245ec60cfde231a8
Arveeed <62373688+Arveeed@users.noreply.github.com>
2023-08-23 16:13:48 +0200

    Update Calculate.py

--- Anwendungsprojekt/API/Calculate.py
+++ Anwendungsprojekt/API/Calculate.py
@@ -17,7 +17,9 @@ temp_mins = []
 temp_maxs = []
 wind_speeds = []
 wind_gusts = []
-weather_icon_codes = []
+min_temps_per_day = []
+max_temps_per_day = []
+icon_urls_per_day = []
 
 # Heutiges Datum
 today = datetime.datetime.now()
@@ -27,50 +29,52 @@ response = requests.get(base_url)
 data = response.json()
 
 # Daten verarbeiten
+current_day = None
+min_temp_day = float('inf')
+max_temp_day = float('-inf')
+icon_urls_day = []
+
 for entry in data['list']:
-            
     # Zeitpunkt des Datensatzes
     timestamp = datetime.datetime.fromtimestamp(entry['dt'])
     
     # Nur Daten für die nächsten 5 Tage berücksichtigen
     if today.date() <= timestamp.date() <= today.date() + datetime.timedelta(days=4):
+        if current_day is None:
+            current_day = timestamp.date()
+        elif current_day != timestamp.date():
+            min_temps_per_day.append(min_temp_day)
+            max_temps_per_day.append(max_temp_day)
+            icon_urls_per_day.append(icon_urls_day)
+            min_temp_day = float('inf')
+            max_temp_day = float('-inf')
+            icon_urls_day = []
+            current_day = timestamp.date()
+        
         temperature = entry['main']['temp']
         temp_min = entry['main']['temp_min']
         temp_max = entry['main']['temp_max']
         wind_speed = entry['wind']['speed']
         wind_gust = entry['wind'].get('gust', 0)  # gust ist nicht immer vorhanden
-        weather_icon_code = entry['weather'][0]['icon']
-
+        weather_icon = entry['weather'][0]['icon']
+        icon_url = f"http://openweathermap.org/img/w/{weather_icon}.png"
+        
         timestamps.append(timestamp)
         temperatures.append(temperature)
         temp_mins.append(temp_min)
         temp_maxs.append(temp_max)
         wind_speeds.append(wind_speed)
         wind_gusts.append(wind_gust)
-        weather_icon_codes.append(weather_icon_code)
+        icon_urls_day.append(icon_url)
+        
+        min_temp_day = min(min_temp_day, temp_min)
+        max_temp_day = max(max_temp_day, temp_max)
 
-        icon_base_url = "http://openweathermap.org/img/w/"
+# Füge die Temperaturen des letzten Tages hinzu
+min_temps_per_day.append(min_temp_day)
+max_temps_per_day.append(max_temp_day)
+icon_urls_per_day.append(icon_urls_day)
 
-        weather_icons = {
-                    '01d': f"{icon_base_url}01d.png",  # clear sky (day)
-                    '01n': f"{icon_base_url}01n.png",  # clear sky (night)
-                    '02d': f"{icon_base_url}02d.png",  # few clouds (day)
-                    '02n': f"{icon_base_url}02n.png",  # few clouds (night)
-                    '03d': f"{icon_base_url}03d.png",  # scattered clouds
-                    '03n': f"{icon_base_url}03n.png",  # scattered clouds
-                    '04d': f"{icon_base_url}04d.png",  # broken clouds
-                    '04n': f"{icon_base_url}04n.png",  # broken clouds
-                    '09d': f"{icon_base_url}09d.png",  # shower rain
-                    '09n': f"{icon_base_url}09n.png",  # shower rain
-                    '10d': f"{icon_base_url}10d.png",  # rain
-                    '10n': f"{icon_base_url}10n.png",  # rain
-                    '11d': f"{icon_base_url}11d.png",  # thunderstorm
-                    '11n': f"{icon_base_url}11n.png",  # thunderstorm
-                    '13d': f"{icon_base_url}13d.png",  # snow
-                    '13n': f"{icon_base_url}13n.png",  # snow
-                    '50d': f"{icon_base_url}50d.png",  # mist
-                    '50n': f"{icon_base_url}50n.png"   # mist
-                }
 # Ergebnisse ausgeben
 for i in range(len(timestamps)):
     print(f"Datum/Uhrzeit: {timestamps[i]}")
@@ -79,6 +83,8 @@ for i in range(len(timestamps)):
     print(f"Max. Temperatur: {temp_maxs[i]}°C")
     print(f"Windgeschwindigkeit: {wind_speeds[i]} m/s")
     print(f"Böengeschwindigkeit: {wind_gusts[i]} m/s")
-    print(f"Wetter-Icon: {weather_icon_codes[i]}")
-    print(f"Weather Icon URL: {weather_icons[weather_icon_codes[i]]}")
     print("-------------------------------")
+
+for i in range(len(min_temps_per_day)):
+    print(f"Wetter-Icon URL: ", icon_urls_day[i])
+    print(f"Tag {i + 1}: Min. Temperatur: {min_temps_per_day[i]}°C, Max. Temperatur: {max_temps_per_day[i]}°C")
