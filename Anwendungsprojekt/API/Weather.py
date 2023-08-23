1751b9b91b63293b34857a13f980d6b7fc5b8e45
LEugster <130568600+LEugster@users.noreply.github.com>
2023-08-23 17:19:24 +0200

    Weather.py

--- Anwendungsprojekt/API/Weather.py
+++ Anwendungsprojekt/API/Weather.py
@@ -14,11 +14,25 @@ try:
         max_temp = data['main']['temp_max']
         min_temp = data['main']['temp_min']
 
+        current_wind_speed = data['wind']['speed']  # Extract wind speed
+        current_humidity = data['main']['humidity']  # Extract humidity
+        weather_description = data['weather'][0]['description']  # Extract weather description
+        weather_icon = data['weather'][0]['icon']  # Extract weather icon code
+
         print(f"Current Temperature in {city_name}: {current_temp}°C")
         print(f"Max Temperature in {city_name}: {max_temp}°C")
         print(f"Min Temperature in {city_name}: {min_temp}°C")
+        print(f"Current Wind Speed in {city_name}: {current_wind_speed} m/s")
+        print(f"Current Humidity in {city_name}: {current_humidity}%")
+        print(f"Weather Description in {city_name}: {weather_description}")
+
+Using the weather icon code to construct the icon URL
+        icon_url = f"http://openweathermap.org/img/w/%7Bweather_icon%7D.png"
+        print(f"Weather Icon URL: {icon_url}")
 
     else:
         print(f"Error: {data['message']}")
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
+
+
