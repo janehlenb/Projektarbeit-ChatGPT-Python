94ded154f8926198994f48987a24809ceabac9ae
Arveeed <62373688+Arveeed@users.noreply.github.com>
2023-08-23 17:24:59 +0200

    Update Calculate.py

--- Anwendungsprojekt/API/Calculate.py
+++ Anwendungsprojekt/API/Calculate.py
@@ -5,10 +5,10 @@ import datetime
 api_key = "8458a13ebaeba1acef15ef61c32b8d4e"
 
 # Die gewünschte Stadt und Ländercode (z.B. "Berlin,de")
-city = "Berlin,de"
+city = "Berlin"
 
 # Die Basis-URL für die OpenWeatherMap API
-base_url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+forecast_url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
 
 # Listen für die einzelnen Daten
 timestamps = []
@@ -21,18 +21,19 @@ min_temps_per_day = []
 max_temps_per_day = []
 icon_urls_per_day = []
 
+
+
 # Heutiges Datum
 today = datetime.datetime.now()
 
 # API-Anfrage senden
-response = requests.get(base_url)
+response = requests.get(forecast_url)
 data = response.json()
 
 # Daten verarbeiten
 current_day = None
 min_temp_day = float('inf')
 max_temp_day = float('-inf')
-icon_urls_day = []
 
 for entry in data['list']:
     # Zeitpunkt des Datensatzes
@@ -45,10 +46,8 @@ for entry in data['list']:
         elif current_day != timestamp.date():
             min_temps_per_day.append(min_temp_day)
             max_temps_per_day.append(max_temp_day)
-            icon_urls_per_day.append(icon_urls_day)
             min_temp_day = float('inf')
             max_temp_day = float('-inf')
-            icon_urls_day = []
             current_day = timestamp.date()
         
         temperature = entry['main']['temp']
@@ -57,7 +56,6 @@ for entry in data['list']:
         wind_speed = entry['wind']['speed']
         wind_gust = entry['wind'].get('gust', 0)  # gust ist nicht immer vorhanden
         weather_icon = entry['weather'][0]['icon']
-        icon_url = f"http://openweathermap.org/img/w/{weather_icon}.png"
         
         timestamps.append(timestamp)
         temperatures.append(temperature)
@@ -65,7 +63,6 @@ for entry in data['list']:
         temp_maxs.append(temp_max)
         wind_speeds.append(wind_speed)
         wind_gusts.append(wind_gust)
-        icon_urls_day.append(icon_url)
         
         min_temp_day = min(min_temp_day, temp_min)
         max_temp_day = max(max_temp_day, temp_max)
@@ -73,7 +70,27 @@ for entry in data['list']:
 # Füge die Temperaturen des letzten Tages hinzu
 min_temps_per_day.append(min_temp_day)
 max_temps_per_day.append(max_temp_day)
-icon_urls_per_day.append(icon_urls_day)
+
+base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+# API-Anfrage senden
+response = requests.get(base_url)
+data = response.json()
+
+# Wetterdaten aus API-Daten extrahieren
+temperature = data['main']['temp']
+humidity = data['main']['humidity']
+wind_speed = data['wind']['speed']
+weather_icon = data['weather'][0]['icon']
+icon_url = f"http://openweathermap.org/img/w/{weather_icon}.png"
+summary = data['weather'][0]['description']
+
+# Ergebnisse in Variablen speichern
+current_temperature = temperature
+current_humidity = humidity
+current_wind_speed = wind_speed
+current_icon_url = icon_url
+current_summary = summary
 
 # Ergebnisse ausgeben
 for i in range(len(timestamps)):
@@ -86,5 +103,10 @@ for i in range(len(timestamps)):
     print("-------------------------------")
 
 for i in range(len(min_temps_per_day)):
-    print(f"Wetter-Icon URL: ", icon_urls_day[i])
     print(f"Tag {i + 1}: Min. Temperatur: {min_temps_per_day[i]}°C, Max. Temperatur: {max_temps_per_day[i]}°C")
+
+print(f"Aktuelle Temperatur: {current_temperature}°C")
+print(f"Luftfeuchtigkeit: {current_humidity}%")
+print(f"Windgeschwindigkeit: {current_wind_speed} m/s")
+print(f"Wetter-Icon URL: {current_icon_url}")
+print(f"Zusammenfassung: {current_summary}")
